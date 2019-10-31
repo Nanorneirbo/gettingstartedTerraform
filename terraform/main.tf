5a8cbf29b72467fee316f78685f2fc75cf9601a5
Nanorneirbo <47759615+Nanorneirbo@users.noreply.github.com>
2019-10-31 22:23:40 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "eu-west-1"
-  access_key = "AKIA2CS5WTAEB35UOK5H"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
 variable "port" {
